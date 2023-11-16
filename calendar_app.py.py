19c23c692d037a03eacda0f7d4c993b251de16f2
RinaBakanovska <iryna.bakanovska@e-u.edu.ua>
2023-11-16 09:38:38 +0200

    Ваш коміт-повідомлення тут

--- calendar_app.py.py
+++ calendar_app.py.py
@@ -1,24 +1,34 @@
 import tkinter as tk
 import datetime
 
-def show_datetime():
-    current_datetime = datetime.datetime.now()
-    datetime_label.config(text=f"Current date and time: {current_datetime}")
+class DateTimeService:
+    @staticmethod
+    def get_current_datetime():
+        return datetime.datetime.now()
 
+class GUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Graphic Calendar")
 
-root = tk.Tk()
-root.title("Graphic Calendar")
+        self.datetime_label = tk.Label(root, text="", font=("Helvetica", 14))
+        self.datetime_label.pack(pady=10)
 
+        self.update_button = tk.Button(root, text="Update", command=self.update_datetime)
+        self.update_button.pack(pady=10)
 
-datetime_label = tk.Label(root, text="", font=("Helvetica", 14))
-datetime_label.pack(pady=10)
+        self.exit_button = tk.Button(root, text="Close", command=self.root.quit)
+        self.exit_button.pack(pady=10)
 
-update_button = tk.Button(root, text="Update", command=show_datetime)
-update_button.pack(pady=10)
+    def update_datetime(self):
+        current_datetime = DateTimeService.get_current_datetime()
+        self.datetime_label.config(text=f"Current date and time: {current_datetime}")
 
-exit_button = tk.Button(root, text="Close", command=root.quit)
-exit_button.pack(pady=10)
+    def show_datetime(self):
+        self.update_datetime()
+        self.root.mainloop()
 
-show_datetime()
-
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    gui = GUI(root)
+    gui.show_datetime()
